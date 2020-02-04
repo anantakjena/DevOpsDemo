276779b402ed6402c5de8ef22f89918ea2468ebc
anantakjena <ananta_jena@hotmail.com>
2020-02-04 10:47:24 +0000

    Add project files.

---
+++ DevOpsDemo/Repository/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DevOpsDemo.Models;
+
+namespace DevOpsDemo.Repository
+{
+    public interface IPostRepository
+    {
+        List<PostViewModel> GetPosts();
+    }
+}
